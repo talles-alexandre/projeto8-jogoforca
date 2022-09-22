5fd9dc7de1a3cf6454cf682dac9f584099dc264e
talles-alexandre <alexandretalles13@gmail.com>
2022-09-22 00:20:20 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import forca0 from "./assets/forca0.png";
+import palavras from "./palavras";
+import alfabeto from "./alfabeto";
+
 export default function App() {
   return (
     <>
-      <h1>Hello word</h1>
+      <div className="top">
+        <div className="telaJogo">
+          <img src={forca0}></img>
+        </div>
+        <div className="escolherPalavra">
+          <button>Escolher Palavras</button>
+        </div>
+      </div>
+
+      <div className="footer">
+        <span> Já sei a palavra</span>
+        <input></input>
+        <button>Chutar</button>
+      </div>
     </>
   );
 }
